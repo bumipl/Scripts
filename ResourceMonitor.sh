76f634727fc94c8abe24800e36fef88c3ba3f39d
bumipl <91955183+bumipl@users.noreply.github.com>
2023-05-24 16:02:13 +0200

    disk_usage_table added

--- ResourceMonitor.sh
+++ ResourceMonitor.sh
@@ -3,29 +3,39 @@ threshold_memory=90
 threshold_cpu=90
 threshold_disk=90
 
+# Variable to store the output
+email_body=""
+
 # Check memory usage
 memory_usage=$(free -m | awk 'NR==2{printf "%.2f", $3*100/$2 }')
 if (( $(echo "$memory_usage > $threshold_memory" | bc -l) )); then
-  echo "Memory usage exceeded the threshold: $memory_usage%"
-  # Add notification code or other actions
+  email_body+="Memory usage exceeded the threshold: $memory_usage%\n"
 else
-  echo "Memory usage is within limits: $memory_usage%"
+  email_body+="Memory usage is within limits: $memory_usage%\n"
 fi
 
 # Check CPU usage
 cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
 if (( $(echo "$cpu_usage > $threshold_cpu" | bc -l) )); then
-  echo "CPU usage exceeded the threshold: $cpu_usage%"
-  # Add notification code or other actions
+  email_body+="CPU usage exceeded the threshold: $cpu_usage%\n"
 else
-  echo "CPU usage is within limits: $cpu_usage%"
+  email_body+="CPU usage is within limits: $cpu_usage%\n"
 fi
 
 # Check disk usage
 disk_usage=$(df -h / | awk 'NR==2{print $5}' | cut -d'%' -f1)
 if (( $disk_usage > $threshold_disk )); then
-  echo "Disk usage exceeded the threshold: $disk_usage%"
-  # Add notification code or other actions
+  email_body+="Disk usage exceeded the threshold: $disk_usage%\n"
 else
-  echo "Disk usage is within limits: $disk_usage%"
+  email_body+="Disk usage is within limits: $disk_usage%\n"
+fi
+
+#Check Disk usage space
+disk_usage_table=$(df -h / /data1 /data2)
+  email_body+="$disk_usage_table\n"
+
+# Send email notification
+if [ -n "$email_body" ]; then
+  echo "Sending email notification..."
+  echo -e "$email_body" | mail -s "System Monitoring Report" szala2002.pl@gmail.com
 fi
