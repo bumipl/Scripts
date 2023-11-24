893d23ed6ebdad8169edcd536bb6b44680287620
bumipl <91955183+bumipl@users.noreply.github.com>
2023-11-24 22:07:24 +0100

    Add files via upload
    This is a scrpit to make bootable USB from an ISO.

---
+++ BootableUSB.sh
@@ -0,0 +1,19 @@
+####################################################
+# This is a scrpit to make bootable USB from an ISO.
+
+#! /bin/bash
+
+# Chose a disk you'd like to make bootable 
+lsblk | egrep "sdc|media"
+read -p "Chose a disk you'd like to make bootable (eg. /dev/sdc): " disk 
+
+# Chose an iso image you'd like to burn
+find ~ -type f -name *.iso -print
+read -p "Chose an iso image you'd like to burn (type whole path): " image
+
+# Final Execution of making bootable USB
+sudo dd if=$image of=$disk bs=16M status=progress oflag=sync
+sudo partprobe $disk 2>/dev/null
+
+# Final message
+echo "Your USb is ready, changes will be visible after reinserting the disk into the computer"
